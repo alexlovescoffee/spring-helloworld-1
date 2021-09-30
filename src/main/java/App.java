a463f85ba3117311e738e0517fd8c74779117399
Achkasov_alexey <alexeyfaker@gmail.com>
2021-09-30 12:57:45 +0300

    Test passed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloWorld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloWorld");
+        Cat cat1 = applicationContext.getBean(Cat.class);
+        Cat cat2 = applicationContext.getBean(Cat.class);
+
+        System.out.println(bean1 == bean2);
+        System.out.println(cat1 == cat2);
     }
 }
